97322fcea8c788fd554829662db7cfacc177c963
Sergey Bovin <snowbovin@gmail.com>
2025-11-09 20:38:15 +0300

    Review fixes

--- backend/src/order/order.controller.ts
+++ backend/src/order/order.controller.ts
@@ -2,7 +2,7 @@ import { Controller, Post, Body } from '@nestjs/common';
 import { OrderService } from './order.service';
 import { CreateOrderDto } from './dto/create-order.dto';
 
-@Controller()
+@Controller('api/afisha')
 export class OrderController {
   constructor(private readonly orderService: OrderService) {}
 
