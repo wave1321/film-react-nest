09a2c2c2e76d74c0aa5a8a6de3d67b91c7341224
Sergey Bovin <snowbovin@gmail.com>
2025-11-26 01:21:02 +0300

    Add mocks

--- backend/src/films/films.service.spec.ts
+++ backend/src/films/films.service.spec.ts
@@ -1,12 +1,26 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { FilmsService } from './films.service';
+import { FilmsRepository } from '../repository/afisha.repository';
 
 describe('FilmsService', () => {
   let service: FilmsService;
 
+  // Создаем мок репозитория
+  const mockFilmsRepository = {
+    findAll: jest.fn(),
+    findById: jest.fn(),
+    findFilmSchedule: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [FilmsService],
+      providers: [
+        FilmsService,
+        {
+          provide: FilmsRepository, // Указываем dependency
+          useValue: mockFilmsRepository, // Используем мок
+        },
+      ],
     }).compile();
 
     service = module.get<FilmsService>(FilmsService);
--- backend/src/order/order.service.spec.ts
+++ backend/src/order/order.service.spec.ts
@@ -1,12 +1,25 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { OrderService } from './order.service';
+import { FilmsRepository } from '../repository/afisha.repository';
 
 describe('OrderService', () => {
   let service: OrderService;
 
+  // Создаем мок репозитория
+  const mockFilmsRepository = {
+    findById: jest.fn(),
+    reserveSeat: jest.fn(),
+  };
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [OrderService],
+      providers: [
+        OrderService,
+        {
+          provide: FilmsRepository, // Указываем dependency
+          useValue: mockFilmsRepository, // Используем мок
+        },
+      ],
     }).compile();
 
     service = module.get<OrderService>(OrderService);
